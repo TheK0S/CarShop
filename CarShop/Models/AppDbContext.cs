b9a98cd143199a269d99e8015f0c0642edee88bf
Kos <turchakkonstantin@gmail.com>
2023-09-30 23:34:34 +0300

    Добавьте файлы проекта.

---
+++ CarShop/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace CarShop.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Car> Cars { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=DESKTOP-K60TA32\\SQLEXPRESS;Database=CarShop;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True;");
+    }
+}
---
+++ CarShop/Models/Car.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarShop.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+    }
+}
